adc4d339a9b92bc2033c528f0244109cbc66f569
mogulsam <theejaguarscomedy@gmail.com>
2024-04-22 05:57:36 -0700

    corrected checker fails

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,9 @@
-# This puppet manifest creates a file names/tmp/school
+# Creates a file
 file { '/tmp/school':
-  ensure         => file,
-  mode           => '0744',
-  owner          => 'www-data',
-  group          => 'www-data',
-  content        => 'I love Puppet\n',
-  ensure_newline => true,
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
